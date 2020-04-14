f8276504102376a0044031e0074165501b526880
Pavel <pavel.com@gmail.com>
2020-04-14 14:33:40 -0400

    Added latest data plots

--- scripts/project.sh
+++ scripts/project.sh
@@ -9,5 +9,5 @@ trap 'err_report $LINENO' ERR
 for f in ../data/*.nc; do
 	t="${f%.nc}.tif"
     echo "Projecting ${f} to ${t}"
-    gdal_translate -a_srs EPSG:4326 netCDF:$f:TWS_monthly  -of GTiff $t
+    gdal_translate -a_srs EPSG:4326 netCDF:$f:TWS_monthly -of GTiff $t
 done
